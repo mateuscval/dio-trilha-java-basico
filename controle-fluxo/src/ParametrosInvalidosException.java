76f84c3809b5f1aaf85ee26e74e9e73bc0af4687
Mateus Amaral <mateuscval@gmail.com>
2025-03-26 13:53:40 -0300

    Projeto antigo removido

--- controle-fluxo/src/ParametrosInvalidosException.java
+++
@@ -1,8 +0,0 @@
-public class ParametrosInvalidosException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-	public ParametrosInvalidosException(String msg) {
-        super(msg);
-    }
-}
